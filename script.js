566422955989c00a306c78fe8154cb66ff083d50
V Sahanashre <145795300+Sahanashre-V@users.noreply.github.com>
2023-09-28 19:45:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+
 /* Iteration 4: Make Everything Work */
+// pikachu - sky, ground, body, ears, cheeks,
+var sky = document.getElementById("sky");
+var buttonsky = document.getElementById("buttonsky");
+buttonsky.oninput = () => {
+//  console.log("My color input is working");
+ sky.style.fill = buttonsky.value;
+};
+var ground = document.getElementById("ground");
+var buttonground = document.getElementById("buttonground");
+buttonground.oninput = () => {
+ //  console.log("My color input is working");
+ground.style.fill = buttonground.value;
+};
+var body = document.getElementById("body");
+var buttonbody = document.getElementById("buttonbody");
+buttonbody.oninput = () => {
+//  console.log("My color input is working");
+ body.style.fill = buttonbody.value;
+};
+var cheeks = document.getElementById("cheeks");
+var buttoncheeks = document.getElementById("buttoncheeks");
+buttoncheeks.oninput = () => {
+ //  console.log("My color input is working");
+ cheeks.style.fill = buttoncheeks.value;
+};
+var ears = document.getElementById("ears");
+var buttonears = document.getElementById("buttonears");
+buttonears.oninput = () => {
+//  console.log("My color input is working");
+ ears.style.fill = buttonears.value;
+};
+
+
